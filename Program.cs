e5c993766377c9fb826bebf66764316a271f38e6
Uddhav Dahatonde <dahatondeuddhava@gmail.com>
2022-09-14 10:32:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment13sept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student=new Student();
+            student.AssignValue();
+            Console.WriteLine(student.DisplayValue ());
+        }
+    }
+}
---
+++ student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment13sept
+{
+    internal class Student
+    {
+        string name;
+        int rollnumber, year;
+        public void AssignValue()
+        {
+            name="Uddhav Dahatonde";
+            rollnumber = 45;
+            year = 2022;
+        }
+        public string DisplayValue()
+        {
+            return $" Student Information is:-> Name ===>{name} rollNumber is  {rollnumber}  year ==>{year}";
+        }
+    }
+}
